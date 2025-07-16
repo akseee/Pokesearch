54dc49a14f47264e10be46414787787b1d12a0f0
Akseenja <klasik1995@bk.ru>
2025-07-16 23:31:50 +0300

    feat: add test for pokemon skeleton card

---
+++ src/entities/pokemon/ui/PokemonCardSkeleton.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import { PokemonSkeletonCard } from './PokemonCardSkeleton';
+
+describe('Pokemon Skeleton Card', () => {
+  describe('Render', () => {
+    test('should render with fallabck data', () => {
+      render(<PokemonSkeletonCard />);
+
+      const title = screen.getByText('Pokemon');
+      const order = screen.getByText('#000');
+
+      expect(title).toHaveTextContent('Pokemon');
+      expect(order).toHaveTextContent('#000');
+
+      const spinner = document.querySelector('[class*="spinner"]');
+      expect(spinner).toBeInTheDocument();
+    });
+  });
+});

ff95d6d2f6fc1456bc9cf7829ee39152ef2f489b
Akseenja <klasik1995@bk.ru>
2025-07-16 23:05:30 +0300

    chore: add libs for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Pokesearch/',
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    globals: true,
+    include: ['/*.test.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      all: true,
+      exclude: [
+        'src//.test.{js,jsx,ts,tsx}',
+        'src/**/.spec.{js,jsx,ts,tsx}',
+        'src/main.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'dist/',
+        'src/vite-env.d.ts',
+        'src//*.d.ts',
+        '/types.ts',
+        '/index.ts',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
 });

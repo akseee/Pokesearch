132f1ea0f753c974419f3b3d28b43eee1c58f16e
Akseenja <klasik1995@bk.ru>
2025-07-24 14:51:24 +0300

    refactor: update cponfigs,add urls for msw

--- src/shared/config/msw/handers.ts
+++ src/shared/config/msw/handers.ts
@@ -1,44 +1,35 @@
 import { http, HttpResponse } from 'msw';
 
-export const handlers = [
-  http.get('https://pokeapi.co/api/v2/pokemon', () => {
-    return HttpResponse.json({
-      count: 3,
-      next: '/next',
-      previous: null,
-      results: [
-        { name: 'pokemon1', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
-        { name: 'pokemon2', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
-        { name: 'pokemon3', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
-      ],
-    });
-  }),
-  http.get('https://pokeapi.co/api/v2/pokemon/:id', ({ params }) => {
-    const { id } = params;
+import {
+  mockPokemonResponse,
+  mockPokemonsPage1,
+  mockPokemonsPage2,
+  mockSpeciesResponse,
+} from '../../lib/mocks';
 
-    if (id === 'notfound') {
+export const handlers = [
+  http.get('https://pokeapi.co/api/v2/pokemon', ({ request }) => {
+    const url = new URL(request.url);
+    const offset = url.searchParams.get('offset');
+
+    if (offset === '20') {
+      return HttpResponse.json(mockPokemonsPage2);
+    }
+
+    return HttpResponse.json(mockPokemonsPage1);
+  }),
+
+  http.get('https://pokeapi.co/api/v2/pokemon-species/:name', () => {
+    return HttpResponse.json(mockSpeciesResponse);
+  }),
+
+  http.get('https://pokeapi.co/api/v2/pokemon/:name', ({ params }) => {
+    const { name } = params;
+
+    if (name === 'notfound') {
       return new Response(null, { status: 404 });
     }
 
-    return HttpResponse.json({
-      name: `pokemon${id}`,
-      id: Number(id),
-      order: Number(id),
-      sprites: {
-        other: {
-          ['official-artwork']: {
-            front_default: `https://pokeapi.co/media/sprites/${id}.png`,
-          },
-          dream_world: {
-            front_default: `https://pokeapi.co/media/dream/${id}.svg`,
-          },
-        },
-      },
-      types: [{ type: { name: 'fire' } }],
-      stats: [
-        { base_stat: 1, stat: { name: 'hp' } },
-        { base_stat: 1, stat: { name: 'attack' } },
-      ],
-    });
+    return HttpResponse.json(mockPokemonResponse);
   }),
 ];
---
+++ src/shared/lib/mocks.ts
@@ -0,0 +1,54 @@
+import type { NamedAPIResource } from '../types/api';
+
+export const mockPokemonResponse = {
+  name: 'pikachu',
+  id: 25,
+  order: 25,
+  sprites: {
+    other: {
+      'official-artwork': { front_default: 'image-url' },
+      dream_world: { front_default: null },
+    },
+  },
+  types: [{ type: { name: 'electric' } }],
+  stats: [
+    { base_stat: 35, stat: { name: 'hp' } },
+    { base_stat: 55, stat: { name: 'attack' } },
+  ],
+};
+
+export const mockSpeciesResponse = {
+  flavor_text_entries: [
+    {
+      flavor_text: 'This is a test description',
+      language: { name: 'en' },
+    },
+  ],
+};
+
+export const mockPokemonSource: NamedAPIResource = {
+  name: 'pikachu',
+  url: 'https://pokeapi.co/api/v2/pokemon/pikachu',
+};
+
+export const mockPokemonsPage1 = {
+  count: 3,
+  next: '/?page=1',
+  previous: null,
+  results: [
+    { name: 'pokemon1', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+    { name: 'pokemon2', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+    { name: 'pokemon3', url: 'https://pokeapi.co/api/v2/pokemon/3/' },
+  ],
+};
+
+export const mockPokemonsPage2 = {
+  count: 3,
+  next: null,
+  previous: '/?page=2',
+  results: [
+    { name: 'pokemon4', url: 'https://pokeapi.co/api/v2/pokemon/4/' },
+    { name: 'pokemon5', url: 'https://pokeapi.co/api/v2/pokemon/5/' },
+    { name: 'pokemon6', url: 'https://pokeapi.co/api/v2/pokemon/6/' },
+  ],
+};

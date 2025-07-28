68d3e63076c0646fdaa928e35ff48a4a8dc8006d
Akseenja <klasik1995@bk.ru>
2025-07-28 19:00:25 +0300

    feat: add context setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
+import { useState, type JSX } from 'react';
 import '../shared/styles';
+import { ThemeContext } from './context/context';
 import { Router } from './router/Router';
 
-export const App = (): React.JSX.Element => {
-  return <Router />;
+export const App = (): JSX.Element => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useState(prefersDark ? 'dark' : 'light');
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Router />
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;

253743457339e98bc9c0cdb3b5a748f1e25def19
Akseenja <klasik1995@bk.ru>
2025-08-17 21:11:52 +0300

    fix: change main page route

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,17 +4,12 @@ import MainPage from '../MainPage';
 export default async function Page({
   searchParams,
 }: {
-  searchParams: { query?: string; page?: string };
+  searchParams: Promise<{ query?: string; page?: string }>;
 }) {
   const param = await searchParams;
   const query = param?.query || undefined;
   const page = param?.page ? parseInt(param.page, 10) : 1;
 
-  try {
-    const data = await fetchManyPokemons({ query, page });
-    return <MainPage initialData={data} query={query || ''} page={page} />;
-  } catch (error) {
-    console.error('Failed to fetch pokemons:', error);
-    return <div>Error loading data</div>;
-  }
+  const data = await fetchManyPokemons({ query, page });
+  return <MainPage initialData={data} query={query || ''} page={page} />;
 }
